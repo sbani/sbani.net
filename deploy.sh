31113fbb63f03563c87b5b7e758ae04f03af45d7
Sufijen Bani <git@sbani.net>
2022-02-23 07:48:08 +0100

    Add permission fixing after deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #/bin/bash
+set -xe
+DEPLOY_PATH=/var/www/netw0rkio/
 
 hugo
 
-rsync -avP --delete public/* h4:/var/www/netw0rkio/
+rsync -avP --delete public/* h4:$DEPLOY_PATH
 
+ssh h4 "chown -R caddy:caddy $DEPLOY_PATH"
