a6928c19e46de0539c568a71c8dcbcb3070bb46b
Sufijen Bani <git@sbani.net>
2022-03-30 10:00:35 +0100

    Fix Deletetion During Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 set -xe
 
-DEPLOY_PATH=/var/www/netw0rkio/
+DEPLOY_PATH=/var/www/netw0rkio
 
+rm -rf public
 hugo
 
-rsync -avP --delete public/* h4:$DEPLOY_PATH
+rsync -avP --delete public/ h4:$DEPLOY_PATH
 
 ssh h4 "chown -R caddy:caddy $DEPLOY_PATH"
